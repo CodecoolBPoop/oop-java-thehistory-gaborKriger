cdca4912e73a365573ce0f863dad8408c44eb94f
gaborKriger <regirck@gmail.com>
2018-11-26 08:43:40 +0100

    Array add, removeWord, size method

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -7,22 +7,45 @@ public class TheHistoryArray implements TheHistory {
     /**
      * This implementation should use a String array so don't change that!
      */
+
     private String[] wordsArray = new String[0];
 
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+        String[] temporaryStorage = text.split(" ");
+        System.arraycopy(
+                wordsArray,0,
+                temporaryStorage,0, wordsArray.length);
+        wordsArray = temporaryStorage;
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        int occurrenceCounter = 0;
+        for (String word : wordsArray) {
+            if (word.equals(wordToBeRemoved)) {
+                occurrenceCounter++;
+            }
+        }
+        String[] temporaryStorage = new String[wordsArray.length - occurrenceCounter];
+        int fillCounter = 0;
+        for (int i = 0; i < wordsArray.length; i++) {
+            if (!wordsArray[i].equals(wordToBeRemoved)) {
+                temporaryStorage[i - fillCounter] = wordsArray[i];
+            } else {
+                fillCounter++;
+            }
+        }
+        wordsArray = temporaryStorage;
     }
+
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsArray.length;
     }
 
     @Override
